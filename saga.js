b5bd7e368823af71469eb54bbda72dbea3e8d0c4
刘俊雄 <15858209984@163.com>
2018-09-06 10:57:55 +0800

    redux =>sage 模版修改

--- saga.js
+++ saga.js
@@ -2,18 +2,23 @@ import * as Act from 'actions';
 import { put, select, call, takeEvery } from 'redux-saga/effects';
 import { callTakeEvery, callTakeLatest } from 'util';
 import * as Fetch from 'lib/server';
-/**
- * 测试用例
- */
-let target_common_methods = function*() {
-  yield callTakeEvery(Act.TARGET_COMMON_METHODS, function*(action) {
-    let data = yield call(Fetch.target_list_init, action.params);
-    if (data) {
-      action.cb && action.cb(data);
+
+
+
+let {{template}} = function* () {
+  yield callTakeEvery(Act.TEST, function* (action) {
+    let data;
+    switch (action.api) {
+      case "list":
+        data = yield call(Fetch.post, action.params);
+        break;
+      default:
+        break;
     }
+    data&&action.cb && action.cb(data);
   });
 };
 
 export default function* rootSaga() {
-  yield [target_common_methods()];
+  yield [{{template}}()];
 }
